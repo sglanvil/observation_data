2b783bb09148f1ff1dc84caec1ccbd99a40180f4
Sasha Glanville <51757958+sglanvil@users.noreply.github.com>
2022-11-29 11:08:51 -0700

    Update download_GPCP.sh

--- download_GPCP.sh
+++ download_GPCP.sh
@@ -2,11 +2,11 @@
 
 # /glade/scratch/sglanvil/GPCP_yaga
 
-for iyear in {1999..2020}; do
+for iyear in {1999..2021}; do
         wget -r -l1 --no-parent -A.nc https://www.ncei.noaa.gov/data/global-precipitation-climatology-project-gpcp-daily/access/${iyear}/
 done
 
-for iyear in {1999..2020}; do
+for iyear in {1999..2021}; do
         echo ${iyear}
         mv /glade/scratch/sglanvil/GPCP_yaga/www.ncei.noaa.gov/data/global-precipitation-climatology-project-gpcp-daily/access/${iyear}/* /glade/scratch/sglanvil/GPCP_yaga/
 done
