406195bc603f2b4f8ecad3547cb0f069d84f0dc2
Sasha Glanville <51757958+sglanvil@users.noreply.github.com>
2022-11-29 11:38:23 -0700

    Update download_GPCP.sh

--- download_GPCP.sh
+++ download_GPCP.sh
@@ -11,9 +11,15 @@ for iyear in {1999..2021}; do
         mv /glade/scratch/sglanvil/GPCP_yaga/www.ncei.noaa.gov/data/global-precipitation-climatology-project-gpcp-daily/access/${iyear}/* /glade/scratch/sglanvil/GPCP_yaga/
 done
 
-# Do this by hand.
-# NOTE: there are 3 files that are corrupt (they are very small, and they have duplicates with different creation dates)
-# Find them, and remove them.
+# There are 3 files that are corrupt (they are very small, and they have duplicates with different creation dates). Find them and remove them.
 # ls -Srql * | head -10
-# rm gpcp_v01r03_daily_d20180401_c20180712.nc gpcp_v01r03_daily_d20171101_c20180207.nc gpcp_v01r03_daily_d20170801_c20171107.nc
+rm gpcp_v01r03_daily_d20180401_c20180712.nc gpcp_v01r03_daily_d20171101_c20180207.nc gpcp_v01r03_daily_d20170801_c20171107.nc
 
+# Make time unlimited. Or maybe just use ncecat next time.
+for ifile in *.nc; do
+        echo ${ifile}
+        ncks --mk_rec_dmn time -O ${ifile} ${ifile}_unlimited.nc
+done
+
+# Concatenate files (lon,lat,time) to make single file.
+ncrcat *unlimited.nc gpcp_v01r03_daily_19990101-20211231.nc
