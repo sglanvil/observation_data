ecf6241847541804a123efa6f98bb739dde8648f
Sasha Glanville <51757958+sglanvil@users.noreply.github.com>
2022-11-29 13:25:39 -0700

    Update loop_ERA5.sh

--- loop_ERA5.sh
+++ loop_ERA5.sh
@@ -7,6 +7,6 @@ for iyear in {1999..2020}; do
         for imonth in {01..12}; do
                 export iyear
                 export imonth
-                qsub -v iyear,imonth /glade/work/sglanvil/CCR/ERA5/ERA5grab.sh
+                qsub -v iyear,imonth /glade/work/sglanvil/CCR/ERA5/download_ERA5.sh
         done
 done
