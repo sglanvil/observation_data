8a0cfb17a0009b420a24b6c2270127d0ea65220f
Sasha Glanville <51757958+sglanvil@users.noreply.github.com>
2022-11-30 10:43:48 -0700

    Create make_GPCP_Mon_file.m

---
+++ make_GPCP_Mon_file.m
@@ -0,0 +1,14 @@
+% November 29, 2022
+
+clear; clc; close all;
+
+file='/glade/scratch/sglanvil/GPCP_yaga/gpcp_v01r03_daily_19990101-20211231.nc';
+
+% ----------------------- interpolate -----------------------
+lon=0:359;
+lat=-90:90;
+
+% ----------------------- calculate clim -----------------------
+
+
+% ----------------------- calculate anom -----------------------
